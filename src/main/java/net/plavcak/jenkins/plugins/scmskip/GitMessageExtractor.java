a3b14aa7fd1ee53efa609bc89bd02001841de141
Zbynek Konecny <zbynek1729@gmail.com>
2024-03-24 22:42:23 +0100

    Consider full message for Git changesets

---
+++ src/main/java/net/plavcak/jenkins/plugins/scmskip/GitMessageExtractor.java
@@ -0,0 +1,14 @@
+package net.plavcak.jenkins.plugins.scmskip;
+
+import hudson.plugins.git.GitChangeSet;
+import hudson.scm.ChangeLogSet;
+
+public class GitMessageExtractor {
+	public static String getFullMessage(ChangeLogSet.Entry entry) {
+		if (entry instanceof GitChangeSet) {
+			return ((GitChangeSet) entry).getComment();
+		} else {
+			return entry.getMsg();
+		}
+	}
+}
